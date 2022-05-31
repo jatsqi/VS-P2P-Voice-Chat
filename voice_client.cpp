a8ebd6836511c0a8f45cacb53870646ca2294f6a
Johannes Quast <johannesquast@outlook.de>
2022-05-31 20:58:57 +0200

    Adjust voice client to new metadata client interface

--- voice_client.cpp
+++ voice_client.cpp
@@ -5,20 +5,21 @@
 #include <QDataStream>
 #include <QNetworkDatagram>
 // ------------------------------------------------------------------------------------------------------------------
-CVoiceClient::CVoiceClient(QObject* parent, CLanMetadataClient *metadataClient, uint16_t port)
-    : QIODevice(parent), m_MetadataClient(metadataClient), m_Port(port)
+CVoiceClient::CVoiceClient(QObject* parent, CLanMetadataClient *metadataClient, QAudioFormat format)
+    : QIODevice(parent), m_MetadataClient(metadataClient), m_Format(format)
 {
+    qDebug() << "Pre Bind";
     m_Socket = new QUdpSocket(this);
-
     QObject::connect(m_Socket, &QUdpSocket::readyRead, this, &CVoiceClient::onSocketReadyRead);
-
-    m_Socket->bind(port);
+    bool b = m_Socket->bind(port());
+    qDebug() << "Bind? " << b;
 }
 
 qint64 CVoiceClient::readData(char *data, qint64 maxlen)
 {
     quint64 len = qMin(maxlen, m_ReadBuffer.size());
     memcpy(data, m_ReadBuffer.data(), len);
+    m_ReadBuffer = QByteArray(m_ReadBuffer.data() + len, m_ReadBuffer.size() - len);
 
     qDebug() << "Reading " << len << " bytes of data :)" << " maxlen: " << maxlen << " buffer: " << m_ReadBuffer.size();
     return len;
@@ -29,18 +30,29 @@ qint64 CVoiceClient::writeData(const char *data, qint64 len)
     QByteArray output;
     QDataStream stream(&output, QIODevice::WriteOnly);
 
-    QString name = "TerraStormDE";
-    stream << name;
-    //stream << m_MetadataClient->username();
-    qDebug() << "Sending DATAGRAM with size " << len;
+    const ChannelMetadata *currentChannel = m_MetadataClient->currentChannel();
+    if (currentChannel == nullptr)
+        return 0;
+
+    stream << m_MetadataClient->username();
     stream << QByteArray(data, len);
 
-    //for(auto &a : m_MetadataClient->usersInCurrentChannel())
-    //{
-    m_Socket->writeDatagram(output, QHostAddress::LocalHost, m_Port);
-    //}
+    for(auto &a : currentChannel->joinedUsers)
+    {
+        if (a.username == m_MetadataClient->username())
+           continue;
+        m_Socket->writeDatagram(output, a.host, a.port);
+
+        qDebug() << "Sending voice data to " << a.username << " (Host: " << a.host << ", Port: " << a.port << ")";
+        qDebug() << "I listen on port " << port();
+    }
 
     return len;
+}
+
+uint16_t CVoiceClient::port() const
+{
+    return m_MetadataClient->prefferedVoicePort();
 }
 
 void CVoiceClient::onSocketReadyRead()
@@ -61,9 +73,9 @@ void CVoiceClient::onSocketReadyRead()
         qDebug() << "Received voice from " << username << " with size " << voiceData.size() << " rdbuffer: " << m_ReadBuffer.size();
     }
 
-    if (m_ReadBuffer.size() > 50000) {
+    if (m_ReadBuffer.size() > m_Format.bytesForDuration(1000000)) {
         emit readyRead();
-        m_ReadBuffer.clear();
+        emit voiceDataReady();
     }
 }
 // ------------------------------------------------------------------------------------------------------------------
--- voice_client.h
+++ voice_client.h
@@ -3,6 +3,7 @@
 // ------------------------------------------------------------------------------------------------------------------
 #include <QObject>
 #include <QIODevice>
+#include <QAudioFormat>
 // ------------------------------------------------------------------------------------------------------------------
 class QUdpSocket;
 class CLanMetadataClient;
@@ -11,14 +12,14 @@ class CVoiceClient : public QIODevice
 {
     Q_OBJECT
 public:
-    CVoiceClient(QObject *parent, CLanMetadataClient *metadataClient, uint16_t port);
+    CVoiceClient(QObject *parent, CLanMetadataClient *metadataClient, QAudioFormat format);
 
     qint64 readData(char *data, qint64 maxlen) override;
     qint64 writeData(const char *data, qint64 len) override;
     qint64 bytesAvailable() const override { return m_ReadBuffer.size() + QIODevice::bytesAvailable(); }
     qint64 size() const override { return m_ReadBuffer.size(); }
 
-    uint16_t port() const { return m_Port; }
+    uint16_t port() const;
 
 public slots:
     void onSocketReadyRead();
@@ -28,7 +29,7 @@ signals:
 
 private:
     CLanMetadataClient *m_MetadataClient;
-    uint16_t m_Port;
+    QAudioFormat m_Format;
     QUdpSocket *m_Socket;
     QByteArray m_ReadBuffer;
 };
